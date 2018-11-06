a1552a076096828cd4beea89964ca44dadd73687
noblemajesty <fredadewole@gmail.com>
2018-11-06 19:00:21 +0100

    add fragments

--- app/src/main/java/com/noblemajesty/newsapplication/views/NewsActivity.kt
+++ app/src/main/java/com/noblemajesty/newsapplication/views/NewsActivity.kt
@@ -4,7 +4,6 @@ import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.ProxyFileDescriptorCallback
 import android.support.design.widget.Snackbar
 import com.noblemajesty.newsapplication.R
 import com.noblemajesty.newsapplication.databinding.ActivityNewsBinding
@@ -18,6 +17,10 @@ class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_news)
+        supportFragmentManager.beginTransaction()
+                .add(R.id.fragmentContainer, NewsFragment())
+                .commit()
+        initializeBottomNavigationBar()
         viewModel = ViewModelProviders.of(this).get(NewsActivityViewModel::class.java)
     }
 
@@ -25,6 +28,42 @@ class NewsActivity : AppCompatActivity() {
         Snackbar.make(binding.newsActivity, text.toUpperCase(), Snackbar.LENGTH_LONG)
                 .setAction("RETRY") { callback() }
                 .show()
+    }
+
+    private fun initializeBottomNavigationBar() {
+        binding.bottomNavigationBar.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.news -> { goToNews();true }
+                R.id.sports -> { goToSports(); true }
+                R.id.food -> { goToFood(); true }
+                R.id.movies -> { goToMovies(); true }
+                else -> true
+            }
+        }
+    }
+
+    private fun goToNews() {
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, NewsFragment())
+                .commit()
+    }
+
+    private fun goToSports() {
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, SportsFragment())
+                .commit()
+    }
+
+    private fun goToFood() {
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, FoodFragment())
+                .commit()
+    }
+
+    private fun goToMovies() {
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, MoviesFragment())
+                .commit()
     }
 
 }
