db15578a838ab3ff12f565c90b8d1838470a1be9
noblemajesty <fredadewole@gmail.com>
2018-11-09 16:23:24 +0100

    [feature Sports Fragment]: display sports news

--- app/src/main/java/com/noblemajesty/newsapplication/views/SportsFragment.kt
+++ app/src/main/java/com/noblemajesty/newsapplication/views/SportsFragment.kt
@@ -1,25 +1,86 @@
 package com.noblemajesty.newsapplication.views
 
 
+import android.arch.lifecycle.ViewModelProviders
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.widget.SwipeRefreshLayout
+import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.noblemajesty.newsapplication.R
+import com.noblemajesty.newsapplication.adapters.Adapter
+import com.noblemajesty.newsapplication.databinding.FragmentSportsBinding
+import com.noblemajesty.newsapplication.models.NYTimesResponse
+import com.noblemajesty.newsapplication.utils.NetworkConnectivity
+import com.noblemajesty.newsapplication.viewmodels.NewsActivityViewModel
+import kotlinx.android.synthetic.main.activity_news.*
 
 /**
  * A simple [Fragment] subclass.
  *
  */
-class SportsFragment : Fragment() {
+class SportsFragment : BaseFragment(), SwipeRefreshLayout.OnRefreshListener {
+
+    private lateinit var binding: FragmentSportsBinding
+    private lateinit var viewModel: NewsActivityViewModel
+    private lateinit var sportsResponse: NYTimesResponse
+    private val sportsAdapter by lazy { Adapter() }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sports, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_sports, container, false)
+        initializeRecyclerView()
+        return binding.root
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProviders.of(activity!!).get(NewsActivityViewModel::class.java)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.sportsSwipeRefresh.setOnRefreshListener(this)
+        getData()
+    }
+
+    private fun initializeRecyclerView() {
+        binding.sportsRecyclerView.apply {
+            setHasFixedSize(true)
+            adapter = sportsAdapter
+            layoutManager = LinearLayoutManager(activity!!, LinearLayoutManager.VERTICAL, false)
+        }
+    }
+
+    override fun onRefresh() {
+        onSwipeRefresh(binding.sportsSwipeRefresh) { makeAPICall() }
+    }
+
+    private fun makeAPICall() {
+        if (NetworkConnectivity(activity!!).isConnected()) {
+            binding.display = true
+            viewModel.fetchSports({
+                sportsResponse = it
+                Log.e("News Response", "$sportsResponse")
+                sportsAdapter.update(sportsResponse.results)
+                binding.display = false
+            }, {
+                displaySnackbar(activity!!.newsActivity, it.message!!, ::makeAPICall)
+            })
+        } else {
+            displaySnackbar(activity!!.newsActivity, "No internet connection", ::makeAPICall) }
+    }
+
+    private fun getData() {
+        viewModel.sports?.let {
+            sportsResponse = it
+            sportsAdapter.update(it.results)
+        } ?: makeAPICall()
+    }
 
 }
