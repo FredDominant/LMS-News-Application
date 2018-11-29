10818a9307ab07066432b008b48762012c607ef3
noblemajesty <fredadewole@gmail.com>
2018-11-29 12:03:31 +0100

    add instrumentation test for News Activity

--- app/src/androidTest/java/com/noblemajesty/newsapplication/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/noblemajesty/newsapplication/ExampleInstrumentedTest.kt
@@ -1,6 +1,9 @@
 package com.noblemajesty.newsapplication
 
 import android.support.test.InstrumentationRegistry
+import android.support.test.espresso.Espresso
+import android.support.test.espresso.assertion.ViewAssertions
+import android.support.test.espresso.matcher.ViewMatchers
 import android.support.test.runner.AndroidJUnit4
 
 import org.junit.Test
